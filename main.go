3d21d61c91f69a7a4345c648ffe3a2fe6a327e54
jupitters <sampaiobale@gmail.com>
2025-09-08 14:23:52 -0300

    testing

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main(){
 	}))
 
 	v1Router := chi.NewRouter()
-	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.HandleFunc("/healthz", handlerReadiness)
 	router.Mount("/v1", v1Router)
 	v1Router.Get("/err", handlerErr)
 
