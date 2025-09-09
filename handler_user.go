8fc7e2679dc7bd8323563dcff14d002669c65bd0
jupitters <sampaiobale@gmail.com>
2025-09-09 01:00:56 -0300

    auth package

--- handler_user.go
+++ handler_user.go
@@ -36,3 +36,7 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 	respondWithJSON(w, 201, databaseUserToUser(user))
 }
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	
+}
---
+++ internal/auth/auth.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// extract api key from header
+// Example: Authorization: ApiKey {apiKey}
+func GetAPIKey(headers http.Header) (string, error) {
+	val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("sem informação de autenticação")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("header de autenticação com arugmentos demais")
+	}
+	if vals[0] != "ApiKey" {
+		return "", errors.New("argumento inexistente no header de autenticação")
+	}
+
+	return vals[1], nil
+}
