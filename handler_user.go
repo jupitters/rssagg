6e0fa12c221daa6ceb9f6083d840b1596cbff577
jupitters <sampaiobale@gmail.com>
2025-09-09 00:42:48 -0300

    migration api_key

--- handler_user.go
+++ handler_user.go
@@ -34,5 +34,5 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, 201, user)
+	respondWithJSON(w, 201, databaseUserToUser(user))
 }
