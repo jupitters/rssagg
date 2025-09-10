4821479b4ef97ff06d4a4ba00c91a06a14167bcf
jupitters <sampaiobale@gmail.com>
2025-09-10 10:52:38 -0300

    get feeds func

--- handler_feed.go
+++ handler_feed.go
@@ -1,41 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
-
-	"github.com/google/uuid"
-	"github.com/jupitters/rssagg/internal/database"
 )
 
-func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
-	type parameters struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	}
-	decoder := json.NewDecoder(r.Body)
-
-	params := parameters{}
-	err := decoder.Decode(&params)
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Erro ao ajustar o json: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Erro ao buscar feeds: %v", err))
 		return
 	}
 
-	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
-		Name:      params.Name,
-		Url:       params.URL,
-		UserID:    user.ID,
-	})
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Erro ao criar o feed: %v", err))
-		return
-	}
-
-	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+	respondWithJSON(w, 201, databaseFeedToFeed(feeds))
 }
