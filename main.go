7c4a8369ab4ac715d0c10b8679f6c0d5088c47cf
jupitters <sampaiobale@gmail.com>
2025-09-11 17:29:04 -0300

    main

--- main.go
+++ main.go
@@ -63,6 +63,8 @@ func main() {
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
