0481a7427affd8e279548bac3695b6866bfd8664
jupitters <sampaiobale@gmail.com>
2025-09-11 12:55:55 -0300

    handler feed follow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -12,7 +12,7 @@ import (
 
 func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
-		FeedID string `json:"feed_id"`
+		FeedID uuid.UUID `json:"feed_id"`
 	}
 	decoder := json.NewDecoder(r.Body)
 
@@ -23,28 +23,17 @@ func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.
 		return
 	}
 
-	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
-		Name:      params.Name,
-		Url:       params.URL,
 		UserID:    user.ID,
+		FeedID:    params.FeedID,
 	})
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Erro ao criar o feed: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Erro ao seguir o feed: %v", err))
 		return
 	}
 
-	respondWithJSON(w, 201, databaseFeedToFeed(feed))
-}
-
-func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
-	feeds, err := apiCfg.DB.GetFeeds(r.Context())
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Erro ao buscar feeds: %v", err))
-		return
-	}
-
-	respondWithJSON(w, 200, databaseFeedsToFeeds(feeds))
+	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 }
