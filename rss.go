f4cbe73afc1e53f1336e0449c27dd24aceed218e
jupitters <sampaiobale@gmail.com>
2025-09-12 14:02:55 -0300

    model rss

---
+++ rss.go
@@ -0,0 +1,22 @@
+package main
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"language"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"descritpion"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func URLtoFeed(url string) (RSSFeed, error) {
+
+}
