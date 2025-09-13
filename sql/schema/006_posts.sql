17a69fc35f81dbdc97b0ac9d9b9f8e29e0f56b3b
jupitters <sampaiobale@gmail.com>
2025-09-13 14:04:01 -0300

    fixed sql

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -6,7 +6,7 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     description TEXT,
     published_at TIMESTAMP NOT NULL,
-    url NOT NULL UNIQUE,
+    url TEXT NOT NULL UNIQUE,
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
 
