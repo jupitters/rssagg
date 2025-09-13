cd77ff210afd585b1affc32cef7ffee585126713
jupitters <sampaiobale@gmail.com>
2025-09-13 14:05:57 -0300

    psots sql

---
+++ sql/queries/posts.sql
@@ -0,0 +1,7 @@
+-- name: CreatePost :one
+INSERT INTO users(id, created_at, updated_at, title, description, published_at, url, feed_id)
+VALUES($1, $2, $3, $4, $5, %6, %7, %8)
+RETURNING *;
+
+-- name: GetUserByApiKey :one
+SELECT * FROM users WHERE api_key = $1;
