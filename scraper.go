8782c3669062eccf226bb16156e752629bf5052f
jupitters <sampaiobale@gmail.com>
2025-09-13 13:22:19 -0300

    scraper

--- scraper.go
+++ scraper.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/jupitters/rssagg/internal/database"
@@ -14,5 +16,32 @@ func startScrapping(
 ) {
 	log.Printf("Scrapping on %v goroutines every %s duration", concurrency, timeBetweenRequests)
 	ticker := time.NewTicker(timeBetweenRequests)
-	for ; ; <- ticker.C
+	for ; ; <-ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(),
+			int32(concurrency),
+		)
+		if err != nil {
+			log.Println("Erro buscando feeds: %v", err)
+			continue
+		}
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
+	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+
+	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Println("Erro marcando o feed como fetched: %v", err)
+		return
+	}
 }
