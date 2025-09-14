48a0c49f129867863a5389ce846a95865a92a019
jupitters <sampaiobale@gmail.com>
2025-09-14 12:27:47 -0300

    fix error

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
@@ -78,6 +79,9 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 				FeedID:      feed.ID,
 			})
 		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
 			log.Println("Falha ao criar o post!", err)
 		}
 
